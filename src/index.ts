aa303ea3cc468cad9b7fc5043514bda5c2414fcd
soulwax <konstantin.kling@braincon.de>
2022-10-16 22:36:13 +0000

    Proper formatting 2

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-require('dotenv').config(); // Recommended way of loading dotenv
-import container from "./inversify.config";
-import {TYPES} from "./types";
-import {Bot} from "./bot";
+require('dotenv').config() // Recommended way of loading dotenv
+import container from './inversify.config'
+import { TYPES } from './types'
+import { Bot } from './bot'
 
-let bot = container.get<Bot>(TYPES.Bot);
+let bot = container.get<Bot>(TYPES.Bot)
 
-
-bot.listen().then(() => {
-  console.log('Logged in!')
-}).catch((error) => {
-  console.log('Oh no! ', error)
-});
+bot
+  .listen()
+  .then(() => {
+    console.log('Logged in!')
+  })
+  .catch((error) => {
+    console.log('Oh no! ', error)
+  })
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,52 +1,63 @@
-import "reflect-metadata";
-import { Container } from "inversify";
-import { TYPES, DiscordIntents } from "./types";
-import { Bot } from "./bot";
-import { Client } from "discord.js";
-import { DiscordIntentBitField } from "./intents";
+import 'reflect-metadata'
+import { Container } from 'inversify'
+import { TYPES, DiscordIntents } from './types'
+import { Bot } from './bot'
+import { Client } from 'discord.js'
+import { DiscordIntentBitField } from './intents'
 const INTENTS = process.env.DISCORD_INTENTS?.split(',') || []
 
-let container = new Container();
-
-
+let container = new Container()
 
 let calculateBulkIntents = (all: boolean, boundary: number) => {
-    let intents = 1
-    let boundaryIntents : number = boundary ? boundary : 0
-    if (all) { // override boundary
-        boundaryIntents = 19
-    }
-    // or all intents
-    for (let i = 0; i < boundaryIntents; i++) {
-        intents |= DiscordIntentBitField[Object.keys(DiscordIntentBitField)[i]]
-    }
-    console.log(`intents calculated: ${intents}`)
-    return intents - 1
+  let intents = 1
+  let boundaryIntents: number = boundary ? boundary : 0
+  if (all) {
+    // override boundary
+    boundaryIntents = 19
+  }
+  // or all intents
+  for (let i = 0; i < boundaryIntents; i++) {
+    intents |= DiscordIntentBitField[Object.keys(DiscordIntentBitField)[i]]
+  }
+  console.log(`intents calculated: ${intents}`)
+  return intents - 1
 }
 
 let calculateSpecificIntents = (intents: string[]) => {
-    let calculatedIntents = 1
-    intents.forEach(intent => {
-        let intentValue : number = DiscordIntentBitField[intent]?.valueOf()
-        calculatedIntents |= intentValue
-    })
-    console.log(`intents calculated: ${calculatedIntents}`)
-    return calculatedIntents - 1
+  let calculatedIntents = 1
+  intents.forEach((intent) => {
+    let intentValue: number = DiscordIntentBitField[intent]?.valueOf()
+    calculatedIntents |= intentValue
+  })
+  console.log(`intents calculated: ${calculatedIntents}`)
+  return calculatedIntents - 1
 }
 
-container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
+container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope()
 
 if (INTENTS[0] === 'all') {
-    container.bind<Client>(TYPES.Client).toConstantValue(new Client({ intents: calculateBulkIntents(true, 0) }));
+  container
+    .bind<Client>(TYPES.Client)
+    .toConstantValue(new Client({ intents: calculateBulkIntents(true, 0) }))
 } else if (INTENTS.length > 1) {
-    container.bind<Client>(TYPES.Client).toConstantValue(new Client({ intents: calculateSpecificIntents(INTENTS) }));
+  container
+    .bind<Client>(TYPES.Client)
+    .toConstantValue(new Client({ intents: calculateSpecificIntents(INTENTS) }))
 } else if (typeof INTENTS[0] === 'number' && INTENTS[0] > 0) {
-    container.bind<Client>(TYPES.Client).toConstantValue(new Client({ intents: calculateBulkIntents(false, parseInt(INTENTS[0])) }));
+  container
+    .bind<Client>(TYPES.Client)
+    .toConstantValue(
+      new Client({ intents: calculateBulkIntents(false, parseInt(INTENTS[0])) })
+    )
 } else {
-    console.warn('No intents specified, defaulting to GUILDS only')
-    container.bind<Client>(TYPES.Client).toConstantValue(new Client({ intents: calculateBulkIntents(false, 0) }));
+  console.warn('No intents specified, defaulting to GUILDS only')
+  container
+    .bind<Client>(TYPES.Client)
+    .toConstantValue(new Client({ intents: calculateBulkIntents(false, 0) }))
 }
 
-container.bind<string>(TYPES.Token).toConstantValue(process.env.DISCORD_BOT_TOKEN ?? '');
+container
+  .bind<string>(TYPES.Token)
+  .toConstantValue(process.env.DISCORD_BOT_TOKEN ?? '')
 
-export default container;
+export default container
